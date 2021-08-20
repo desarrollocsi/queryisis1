9ae0e7f08773ad57a1387dfc0c3815ddf81d0a5e
yur <adanyur@gmail.com>
2021-08-20 14:42:16 -0500

    se renombro el nombre del with,modificacion de campo con alias numeroCorrelativo

--- function/function_c_generacion_numero_lote_tedef.sql
+++ function/function_c_generacion_numero_lote_tedef.sql
@@ -7,18 +7,19 @@ DECLARE numeroDeLote INTEGER;
 
 BEGIN
 			
-	WITH generaNumeroDeLote AS (
-		SELECT 
-				cesgrs::TEXT ~ '30' AS esNoConsiderado,
-				cesgrs AS codigoDeIafa,
-				(COALESCE(CASE WHEN lote = 0 OR  substr(lote::TEXT,2) LIKE '' THEN 0 END,
-				substr(lote::TEXT,2)::INT) + 1)::TEXT AS numeroCorrelativo
-		FROM pwccp04 
-		WHERE cesgrs=codigoDeIafa
-		ORDER BY fcrgstro DESC LIMIT 1
-	)SELECT (COALESCE(CASE WHEN esNoConsiderado THEN '' END,a.codigoDeIafa::TEXT)||LPAD(numeroCorrelativo,5,'0'))::INT
-	INTO numeroDeLote
-	FROM generaNumeroDeLote as a;
+WITH generaNumeroDeLote AS (
+	SELECT 
+			cesgrs::TEXT ~ '30' AS esNoConsiderado,
+			cesgrs AS codigoDeIafa,
+			(COALESCE(CASE WHEN lote = 0 THEN 0 END,
+					COALESCE(CASE WHEN substr(lote::TEXT,2) LIKE '' THEN lote::TEXT END, 
+					substr(lote::TEXT,2))::INT) + 1)::TEXT AS numeroCorrelativo
+	FROM pwccp04
+	WHERE cesgrs=codigoDeIafa
+	ORDER BY fcrgstro DESC LIMIT 1
+)SELECT (COALESCE(CASE WHEN esNoConsiderado THEN '' END,a.codigoDeIafa::TEXT)||LPAD(numeroCorrelativo,5,'0'))::INT
+INTO numeroDeLote
+FROM generaNumeroDeLote as a;
 	
 	
 	RETURN numeroDeLote;
