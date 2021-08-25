e7e8a8a6cfe33d6a34284f91289feac52570fe34
yur <adanyur@gmail.com>
2021-08-25 15:31:39 -0500

    se agrego query para validar el tipo de operacion a titulo gratuito con la LATERAL de PG

--- function/function_fc_q_registro_venta_comprobante.sql
+++ function/function_fc_q_registro_venta_comprobante.sql
@@ -20,13 +20,16 @@ BEGIN
   d_fecha2 := to_date(p_fecha2, 'YYYY-MM-DD'); 
   for data in 	
     SELECT pwtgn00.nprfctra,pwtgn00.tcmprbnte,pwtgn00.nscmprbnte,pwtgn00.ncmprbnte,to_char(pwtgn00.femsn,'dd/mm/yyyy'), 
-    case pwtgn00.ctdcmnto when 0 then 'SD' when 1 then 'DNI' when 2 then 'CE' when 3 then 'RUC' when 4 then 'CIP' when 5 then 'PAS' when 7 then 'TUTOR' when 8 then 'RUC' end tip_doc,
-    --sgatv01.catdcmnto as tip_doc,
+    case pwtgn00.ctdcmnto when 0 then 'SD' when 1 then 'DNI' when 2 then 'CE' when 3 then 'RUC' when 4 then 'CIP' when 5 then 'PAS' when 7 then 'TUTOR' when 8 then 'RUC' end tip_doc,    
     coalesce(pwtgn00.ndidntdd,'') as nro_doc, 
-    --trim(pwtgn00.rsaynmbrs) as nom_cliente,
     replace(replace(replace(replace(replace(replace(pwtgn00.rsaynmbrs,'Á','A'),'É','E'),'Í','I'),'Ó','O'),'Ú','U'),'?','Ñ')  AS nom_cliente,
-    --(select case when count(*) > 0 then 'CONSULTA AMBULATORIA' else 'CONSUMO' end from pwccp01 where pwccp01.nprfctra = pwtgn00.nprfctra and pwccp01.cprcdmnto = 29) as cons_amb,
-    case when a.nprfctra is not null then 'CONSULTA AMBULATORIA' else 'CONSUMO' end as cons_amb,
+
+    CASE
+	WHEN a.nprfctra is not null THEN 'CONSULTA AMBULATORIA'
+	WHEN tipoDeVenta.esTituloGratuito THEN 'CONSUMO - TITULO GRATUITO'
+	ELSE 'CONSUMO' 
+	END AS cons_amb,
+
     pwtgn00.cmnda, pwtgn00.vvnta, pwtgn00.migv, pwtgn00.imprdndo, pwtgn00.tcta,
     case when pwtgn00.scpgo = 'N' then 'VIGENTE' when pwtgn00.scpgo = 'A' then 'ANULADO' end as estado,
     case when pwtgn00.tcmprbnte = 'N' then (SELECT doc_refer.tcmprbnte FROM pwtgn00 doc_refer WHERE pwtgn00.corcmprbnte = doc_refer.crcmprbnte) end as ref_tipo,
@@ -36,11 +39,9 @@ BEGIN
     'C' as frm_pago, pwtgn00.ccja,pwtgn00.fecha_envio,pwtgn00.fecha_devolucion,
 	coalesce(pwtgn00.lote,0) lote
     FROM pwtgn00 inner join sgatv01 on(pwtgn00.ctdcmnto = sgatv01.ctdcmnto)
-    LEFT JOIN ( select DISTINCT pwccp01.nprfctra
-    from pwccp01
-    where pwccp01.cprcdmnto = 29 --CONSULTA AMBULATORIA
-      ) a ON(
-    a.nprfctra = pwtgn00.nprfctra)
+    LEFT JOIN (select DISTINCT pwccp01.nprfctra
+    from pwccp01 where pwccp01.cprcdmnto = 29 /*CONSULTA AMBULATORIA*/) a ON(a.nprfctra = pwtgn00.nprfctra),
+    LATERAL (SELECT b.tvnta LIKE 'T' AS esTituloGratuito FROM pwccp00 b WHERE  b.clcl=1 AND b.cemprsa =1 AND b.nprfctra=pwtgn00.nprfctra) AS tipoDeVenta
     WHERE  cast(pwtgn00.femsn as date) between d_fecha1 and d_fecha2 
     and pwtgn00.nscmprbnte > 0
 	AND pwtgn00.ncmprbnte > 0
